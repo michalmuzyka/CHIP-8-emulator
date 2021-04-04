adc3aec4927357816a4bc26c49ba7f6bd8b4dca9
michalmuzyka <michal.muzyka@o2.pl>
2021-04-05 00:12:06 +0200

    Minor changes

--- Miki/Miki.cpp
+++ Miki/Miki.cpp
@@ -1,6 +1,7 @@
 ﻿#define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <windows.h>
+#include "Window.h"
 #include "CHIP8/Emulation.hpp"
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
@@ -11,9 +12,15 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
+    MikiWindow miki(hInstance);
+    MSG msg;
+    BOOL gmResult;
+    while ((gmResult = GetMessageW(&msg, nullptr, 0, 0)) != 0) {
+        if (gmResult == -1)
+            CHIP8::log(CHIP8::MESSAGE_TYPE::LOG_ERROR, "Winapi error code: " + std::to_string(GetLastError()));
+        TranslateMessage(&msg);
+        DispatchMessageW(&msg);
+    }
 
-
-
-
-    return 0;
+    return msg.wParam;
 }
--- Miki/Window.cpp
+++ Miki/Window.cpp
@@ -1,21 +1,76 @@
 #include "Window.h"
+#include <tuple>
+#include "CHIP8/Emulation.hpp"
 
-
-bool Window::is_registered_class(HINSTANCE hInst, LPCWSTR cName) {
-
-
+bool MikiWindow::is_registered_class(HINSTANCE hInst, LPCWSTR cName) {
+    WNDCLASSEX wcx;
+    return GetClassInfoExW(hInst, cName, &wcx);
 }
 
-
-void Window::register_class(HINSTANCE hInst, LPCWSTR cName) {
-	WNDCLASSEXW wcx{};
-	wcx.cbSize = sizeof(wcx);
-	wcx.style = CS_VREDRAW | CS_HREDRAW;
-	wcx.lpfnWndProc = window_proc;
-	wcx.hCursor = LoadCursorW(nullptr, IDC_ARROW);
-	wcx.hbrBackground = static_cast<HBRUSH>(GetStockObject(WHITE_BRUSH));
-	wcx.lpszClassName = cName;
-	RegisterClassExW(&wcx);
+void MikiWindow::register_class(LPCWSTR cName) {
+    WNDCLASSEXW wcx{};
+    wcx.cbSize = sizeof(wcx);
+    wcx.style = CS_VREDRAW | CS_HREDRAW;
+    wcx.lpfnWndProc = window_proc;
+    wcx.hCursor = LoadCursorW(nullptr, IDC_ARROW);
+    wcx.hbrBackground = static_cast<HBRUSH>(GetStockObject(WHITE_BRUSH));
+    wcx.lpszClassName = cName;
+    ATOM result = RegisterClassExW(&wcx);
+    if(!result)
+        CHIP8::log(CHIP8::MESSAGE_TYPE::LOG_ERROR,
+            "WinAPI window class cannot be registered, error code: " + std::to_string(GetLastError()));
 }
 
+MikiWindow::MikiWindow(HINSTANCE hIns)
+    :my_hwnd{ nullptr }, hInstance(hIns) {
+    
+    if (!is_registered_class(hInstance, class_name.c_str()))
+        register_class(class_name.c_str());
 
+    HWND result = CreateWindowExW(0, class_name.c_str(), title.c_str(), WS_OVERLAPPED | WS_SYSMENU | WS_CAPTION | WS_THICKFRAME | WS_VISIBLE,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, nullptr, nullptr, hInstance,
+        reinterpret_cast<LPVOID>(this));
+
+    if(!result)
+        CHIP8::log(CHIP8::MESSAGE_TYPE::LOG_ERROR, 
+            "WinAPI window cannot be initialized, error code: " + std::to_string(GetLastError()));
+}
+
+LRESULT MikiWindow::window_proc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    MikiWindow* window = nullptr;
+    if (msg == WM_NCCREATE) {
+        const LPCREATESTRUCTW pcs = reinterpret_cast<LPCREATESTRUCTW>(lParam);
+        window = static_cast<MikiWindow*>(pcs->lpCreateParams);
+        SetWindowLongPtrW(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(window));
+        window->my_hwnd = hWnd;
+    } else window = reinterpret_cast<MikiWindow*>(GetWindowLongPtrW(hWnd, GWLP_USERDATA));
+    
+    if (window) {
+        const LRESULT result = window->window_proc(msg, wParam, lParam);
+        if (msg == WM_NCDESTROY) {
+            window->my_hwnd = nullptr;
+            SetWindowLongPtrW(hWnd, GWLP_USERDATA, 0);
+        }
+        return result;
+    }
+    
+    return DefWindowProcW(hWnd, msg, wParam, lParam);
+}
+
+LRESULT MikiWindow::window_proc(UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg){
+        case WM_CLOSE:
+            DestroyWindow(my_hwnd);
+            return 0;
+        case WM_DESTROY:
+            PostQuitMessage(EXIT_SUCCESS);
+            return 0;
+        default:
+            return DefWindowProcW(my_hwnd, msg, wParam, lParam);
+    }
+}
+
+MikiWindow::~MikiWindow() {
+    if (my_hwnd)
+        DestroyWindow(my_hwnd);
+}
